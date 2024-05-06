b2b526a0ce7043dc1ac4805bcaf59c52b79c7386
TylerKleinbauer <119420696+TylerKleinbauer@users.noreply.github.com>
2024-05-06 16:01:17 +0200

    Update streamlit_tests.py

--- streamlit_tests.py
+++ streamlit_tests.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
+#Title for this test app
+st.title("Streamlit Text Input and Display")
+
+#Initialize the variable for text input
 question = st.text_area("Please type your question here")
 
-placeholder=st.empty()
-placeholder.text(question)
-st.text("Hello")
-
-def add_text_block():
-  if question:
-    st.text("test")
+if question:
+  st.write("user:")
+  st.write(question)
